e2303080b11b743f1374a6dbd5db93b653c56623
Ridhan Bholakiya <ridhanbholakiya198@gmail.com>
2026-08-12 20:49:45 +0530

    Update Nova Arcade Collection

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.novaarcade.app',
+  appName: 'Nova Arcade',
+  webDir: 'dist'
+};
+
+export default config;
